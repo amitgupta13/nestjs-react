3acca1e87f483829d963167926c64bc528f776b7
amitgupta13 <amitgupta13@users.noreply.github.com>
2021-06-01 14:35:59 +0000

    deploy: c43893ce0687362b5a011b6abf29e44ff4c3f5ba

---
+++ precache-manifest.4e0659f754a081a6b0301cd567ef1067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c526eb04cf5f121fd38032dc80fb1d0f",
+    "url": "https://amitgupta13.github.io/nestjs-react/index.html"
+  },
+  {
+    "revision": "a056a973b7ce16274071",
+    "url": "https://amitgupta13.github.io/nestjs-react/static/css/main.492f871e.chunk.css"
+  },
+  {
+    "revision": "b8fbcc031786cc4adc8b",
+    "url": "https://amitgupta13.github.io/nestjs-react/static/js/2.74bcbb58.chunk.js"
+  },
+  {
+    "revision": "a056a973b7ce16274071",
+    "url": "https://amitgupta13.github.io/nestjs-react/static/js/main.05fddda4.chunk.js"
+  },
+  {
+    "revision": "2f065f1f851b390b66df",
+    "url": "https://amitgupta13.github.io/nestjs-react/static/js/runtime~main.575bb1d2.js"
+  }
+]);
